c24f10efed59b31ac13e53787a3b2f9b0ed46b04
arpit agrawal <45744766+arpit1012@users.noreply.github.com>
2022-06-10 11:21:31 +0530

    Create reverse.cpp

---
+++ 1.array/reverse.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0;i<n;i++) cin>>v[i];
+        
+        reverse(v.begin(),v.end());
+        for(auto i:v){
+            cout<<i<<" ";
+            
+        }
+    }
+    
+    
+	//code
+	return 0;
+}
